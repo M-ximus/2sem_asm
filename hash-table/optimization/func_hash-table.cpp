df011c53e83767649b336461f9765510ab827c4f
Maximus814 <Davydov.MA@phystech.edu>
2019-05-01 20:53:21 +0300

    With optimization. It faster than -O1)))))

---
+++ hash-table/optimization/func_hash-table.cpp
@@ -0,0 +1,362 @@
+//
+// Created by max on 21.04.19.
+//
+
+#include "func_hash-table.h"
+unsigned long Accumulator_for_average_ticks = 0;
+
+int laba(const char* way_to_dictionary, const char* exel)
+{
+    assert(way_to_dictionary != nullptr);
+    assert(exel != nullptr);
+
+    str*  strings_table = Fill_the_Addresses(way_to_dictionary);
+    if (strings_table == nullptr)
+    {
+        Printf("Bad file with dictionary - %s", way_to_dictionary);
+        return 7;
+    }
+    char* clean_address = strings_table[0].String;
+
+    FILE* output = fopen(exel, "wb");
+    if (output == nullptr)
+    {
+        Printf("Bad path %s", exel);
+        return 7;
+    }
+
+    my_pointer_List** hash_table = nullptr;
+    try
+    {
+        hash_table = new my_pointer_List* [Size_hash_table];
+    }
+    catch(const std::bad_alloc& elem)
+    {
+        Printf("%s in laba\n", elem.what());
+        return 42;
+    }
+    make_lab(strings_table, hash_table, output);
+
+    fclose(output);
+    cleanMemory(strings_table, &clean_address);
+
+    return 0;
+}
+
+size_t nanoogle(const char* way_to_dictionary, const char* way_to_requests, unsigned int (*func_for_calc_hash)(const char*))
+{
+    assert(way_to_dictionary != nullptr);
+    assert(way_to_requests != nullptr);
+    assert(func_for_calc_hash != nullptr);
+
+    str* array_of_expressions = Fill_the_Addresses(way_to_dictionary);
+    if (array_of_expressions == nullptr)
+    {
+        Printf("Bad file with dictionary - %s", way_to_dictionary);
+        return 0xffffffffffffffff;
+    }
+    char* dict_buff = array_of_expressions[0].String;
+
+    str* requests = Fill_the_Addresses(way_to_requests);
+    if (requests == nullptr)
+    {
+        Printf("Bad file with dictionary - %s", way_to_dictionary);
+        return 0xffffffffffffffff;
+    }
+    char* req_buff = requests[0].String;
+
+    my_pointer_List** hash_table = nullptr;
+    try
+    {
+        hash_table = new my_pointer_List* [Size_hash_table];
+    }
+    catch(const std::bad_alloc& elem)
+    {
+        Printf("In nanoogle %s", elem.what());
+        return 0xfeeeeeeeeeeeeeee;
+    }
+    fill_hash_table(array_of_expressions, hash_table, func_for_calc_hash);
+
+    size_t num_requests = 0;
+    for(size_t i = 0; i < Search_cicle; i++)
+        num_requests += searching_machine(hash_table, requests, func_for_calc_hash);
+
+    printf("%lu\n", Accumulator_for_average_ticks / num_requests);
+
+    delete_hash_info(hash_table);
+    cleanMemory(array_of_expressions, &dict_buff);
+    cleanMemory(requests, &req_buff);
+
+    delete[] hash_table;
+
+    return num_requests;
+}
+
+int fill_hash_table(str* arr, my_pointer_List** hash_table, unsigned int (*hash_func)(const char*))
+{
+    ASSERT(arr != nullptr);
+    ASSERT(hash_table != nullptr);
+
+    try
+    {
+        for (int i = 0; i < Size_hash_table; i++)
+            hash_table[i] = List_create();
+    }
+    catch(const std::exception& elem)
+    {
+        PRINTF("%s in %s->", elem.what(), __PRETTY_FUNCTION__);
+        throw;
+    }
+
+    size_t i = 0;
+    while(arr[i].String != nullptr)
+    {
+        int index = (*hash_func)(arr[i].String) % Size_hash_table;
+        new_head_of_List(hash_table[index], arr[i].String);
+        i++;
+    }
+
+    return 0;
+}
+
+void print_hash_table_stat(my_pointer_List** hash_table, FILE* out)
+{
+    if (out == nullptr)
+    {
+        PRINTF("FILE* out file = nullptr in print_hash_table");//TODO exception?
+        return;
+    }
+
+    for(int i = 0; i < Size_hash_table; i++)
+        fprintf(out, "%lu,\n", hash_table[i]->counter);
+}
+
+
+void make_lab(str* strings_arr, my_pointer_List** hash_table, FILE* out)
+{
+    if (strings_arr == nullptr || hash_table == nullptr)
+    {
+        PRINTF("Bad args");
+        return;
+    }
+
+    typedef unsigned int (*hash_function) (const char*);
+    hash_function arr_hash[Num_hash_func] ={Hash_const, Hash_first_ASCII, Hash_sum_ASCII, Hash_length, Hash_average_ASCII, Hash_GNU};
+
+    for(int i = 0; i < Num_hash_func; i++)
+    {
+        fill_hash_table(strings_arr, hash_table, arr_hash[i]);
+        print_hash_table_stat(hash_table, out);
+        delete_hash_info(hash_table);
+    }
+}
+
+void delete_hash_info(my_pointer_List** hash_table)
+{
+    if (hash_table == nullptr)
+    {
+        PRINTF("Died hash-table\n");
+        return;
+    }
+
+    for(int i = 0; i < Size_hash_table; i++)
+        List_destruct(hash_table + i);
+}
+
+unsigned int Hash_const(const char* word)
+{
+    return 42;
+}
+
+unsigned int Hash_first_ASCII(const char* word)
+{
+    if (word != nullptr)
+        return (unsigned int) word[0];
+    else
+    {
+        PRINTF("zero word\n");
+        return 0;
+    }
+}
+
+unsigned int Hash_sum_ASCII(const char* word)
+{
+    if (word != nullptr)
+    {
+        int i = 0;
+        unsigned int sum = 0;
+        while(word[i] != '\0' && word[i] != ':')
+        {
+            sum += word[i];
+            i++;
+        }
+
+        return sum;
+    }
+    else
+    {
+        PRINTF("zero word\n");
+        return 0;
+    }
+}
+
+unsigned int Hash_length(const char* word)
+{
+    if (word != nullptr)
+    {
+        unsigned int counter = 0;
+        while(word[counter] != '\0' && word[counter] != ':')
+            counter++;
+        return counter;
+    }
+    else
+    {
+        PRINTF("zero word\n");
+        return 0;
+    }
+}
+
+unsigned int Hash_average_ASCII(const char* word)
+{
+    if (word != nullptr)
+    {
+        unsigned int i = 0;
+        unsigned int sum = 0;
+        while(word[i] != '\0' && word[i] != ':')
+        {
+            sum += word[i];
+            i++;
+        }
+        if (i == 0)
+            return 0;
+        else
+            return sum / i;
+    }
+    else
+    {
+        PRINTF("zero word\n");
+        return 0;
+    }
+}
+
+unsigned int Hash_GNU(const char* word)
+{
+    if (word != nullptr)
+    {
+        unsigned int hash = 0;
+        unsigned int i = 0;
+        while(word[i] != '\0' && word[i] != ':')
+        {
+            hash = hash * 33 + word[i];
+            i++;
+        }
+        return hash;
+    }
+    else
+    {
+        PRINTF("zero word\n");
+        return 0;
+    }
+}
+
+List_pointer_Elem* Find_elem(my_pointer_List* List, const char* request)
+{
+    if (List == nullptr || request == nullptr)
+    {
+        Printf("Bad arguments in request\n");
+        return nullptr;
+    }
+
+    List_pointer_Elem* current = nullptr;
+    if (List->head != nullptr)
+        current = List->head;
+    else
+    {
+        Printf("Empty list\n");
+        return nullptr;
+    }
+
+    unsigned long sum_time = 0;
+    int i = 0;
+    for(; i < List->counter; i++)
+    {
+        unsigned long start_time = curr_tick_num();
+        bool check = compare_req(current->data, request);
+        sum_time = (curr_tick_num() - start_time) + sum_time;
+
+        if (check)
+            break;
+
+        if (current->next == nullptr)
+            return nullptr;
+
+        current = current->next;
+    }
+    i++;
+    printf("%lu\n", sum_time / i);
+    return current;
+}
+
+bool compare_req(const char* str_for_comp, const char* request)
+{
+    if (str_for_comp == nullptr || request == nullptr)//TODO kill first check
+    {
+        Printf("Bad arg for comparator\n");
+        return false;
+    }
+
+
+    asm(".intel_syntax noprefix");
+    //asm("xor rcx, rcx");
+    asm("mov rdx, [rdi]");
+    asm("mov rax, [rsi]");
+    asm("cmp al, dl");
+    asm("je .Maybe_equal");
+    asm("xor rax, rax");
+    asm("pop rbp");
+    asm("ret");
+    asm(".Maybe_equal:");
+    asm(".att_syntax prefix");
+
+    int i = 1;
+    while(str_for_comp[i] == request[i] && str_for_comp[i] != '\0' && str_for_comp[i] != ':' && request[i] != '\0')
+    {
+        i++;
+    }
+
+    if (request[i] == '\0' && (str_for_comp[i] == ':' || str_for_comp[i] == '\0'))//TODO check listing
+        return true;
+    else
+        return false;
+
+}
+
+int searching_machine(my_pointer_List** hash_table, str* requests, unsigned int (*func_for_calc_hash)(const char*))
+{
+    assert(hash_table != nullptr);
+    assert(requests != nullptr);
+    assert(func_for_calc_hash != nullptr);
+
+
+    unsigned int hash = 0;
+    int i = 0;
+    while(requests[i].String != nullptr)
+    {
+        //printf("%p %d %s\n", requests[i].String, requests[i].Length, requests[i].String);
+        hash = (*func_for_calc_hash)(requests[i].String) % Size_hash_table;
+        if(!Find_elem(hash_table[hash], requests[i].String))
+            printf("Bad research\n");
+        i++;
+    }
+
+    return i;
+}
+
+void curr_tick_reg(unsigned int* high, unsigned int* low)
+{
+    asm (   "RDTSC\n\t"
+            "movl %[H], %%edx\n\t"
+            "movl %[L], %%eax\n\t"
+            :
+            :[H]"m"(*high), [L]"m"(*low));
+}
---
+++ hash-table/optimization/lib_for_hack.h
@@ -0,0 +1,69 @@
+//
+// Created by max on 08.04.19.
+//
+
+#ifndef HACK_TEMA_LIB_FOR_HACK_H
+#define HACK_TEMA_LIB_FOR_HACK_H
+
+#include <cstdio>
+#include <cstring>
+#include <cassert>
+#include <cmath>
+#include <cstdlib>
+
+#ifdef Meow
+#define PRINTF printf
+#else
+#define PRINTF
+#endif
+
+
+
+const char way_to_in[] = {"/home/max/CLionProjects/hash-table/dictUTF.txt"};
+const char way_to_out[] = {"/home/max/CLionProjects/hash-table/new_text.txt"};
+
+struct str {
+    char*         String;
+    unsigned long Length;
+};
+
+
+
+//----------------------------------------------------------------------------------------------------------------------
+//! Buffer\n\n
+//! Here we just fill the Buffer.
+//! Very imortant to put '\0' in the end of Buffer for working with it.
+//----------------------------------------------------------------------------------------------------------------------
+
+char* Fill_the_Buffer(const char* nameInput, unsigned int* NumStrings = nullptr);
+
+//----------------------------------------------------------------------------------------------------------------------
+//! Size\n\n
+//! Before we will create the Buffer, we need to know his size.
+//! Buffer size - file size.
+//----------------------------------------------------------------------------------------------------------------------
+
+long int Size(FILE* InputFile);
+
+//----------------------------------------------------------------------------------------------------------------------
+//! numberSymbol\n\n
+//! Mini-function for counting any symbol in the text.
+//----------------------------------------------------------------------------------------------------------------------
+
+unsigned int numberSymbol(const char* Buffer, char Symbol);
+
+str* Fill_the_Addresses(const char* nameInput);
+
+//----------------------------------------------------------------------------------------------------------------------
+//! cleanMemory\n\n
+//! Mini-function to clear memory.
+//----------------------------------------------------------------------------------------------------------------------
+
+void cleanMemory(str* Addresses, char** CP_Addresses_of_Strings);
+
+void Fill_the_OutputFile(str* Addresses, const char* nameOutput);
+
+unsigned long curr_tick_num();
+
+
+#endif //HACK_TEMA_LIB_FOR_HACK_H
